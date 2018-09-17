2d198f79880f880573518e3553f589fb8342c20c
pelegs <pelegs@gmail.com>
2018-09-17 19:28:01 +0200

    Basically works

--- lib/main.py
+++ lib/main.py
@@ -4,22 +4,26 @@ Main library for moldraw
 
 import svgwrite
 import numpy as np
+from rdkit import Chem
+from rdkit import Geometry
+from rdkit.Chem import AllChem
+from rdkit.Chem import Draw
 from lib.math import *
 
 atom_dict = {
-    'H':  [1,  1.00, 'rgb(182,225,255)', 'rgb(176,248,251)', 'black',  .01],
-    'B':  [5,  1.30, 'rgb(255,158,158)', 'rgb(255,179,179)', 'black',  .02],
-    'C':  [6,  1.23, 'rgb(128,128,128)', 'rgb(179,179,179)', 'black',  .00],
-    'N':  [7,  1.54, 'rgb(128,000,128)', 'rgb(255,000,255)', 'white',  .00],
-    'O':  [8,  1.77, 'rgb(212,000,000)', 'rgb(255,085,085)', 'black',  .00],
-    'F':  [9,  1.80, 'rgb(098,146,000)', 'rgb(143,160,085)', 'black',  .05],
-    'Si': [14, 1.85, 'rgb(215,254,220)', 'rgb(200,200,200)', 'black', -.05],
-    'P':  [15, 1.95, 'rgb(010,154,000)', 'rgb(046,189,070)', 'black',  .05],
-    'S':  [16, 2.15, 'rgb(238,206,000)', 'rgb(253,196,000)', 'black',  .00],
-    'Cl': [17, 2.25, 'rgb(140,205,000)', 'rgb(143,200,085)', 'black', -.10],
-    'Fe': [26, 2.25, 'rgb(140,140,140)', 'rgb(200,200,209)', 'black', -.10],
-    'Br': [35, 2.35, 'rgb(230,085,000)', 'rgb(243,160,085)', 'black', -.10],
-    'I':  [53, 2.55, 'rgb(175,000,205)', 'rgb(215,000,165)', 'black',  .10],
+    1:  ['H',  1.00, 'rgb(182,225,255)', 'rgb(176,248,251)', 'black',  .01],
+    5:  ['B',  1.30, 'rgb(255,158,158)', 'rgb(255,179,179)', 'black',  .02],
+    6:  ['C',  1.23, 'rgb(128,128,128)', 'rgb(179,179,179)', 'black',  .00],
+    7:  ['N',  1.54, 'rgb(128,000,128)', 'rgb(255,000,255)', 'white',  .00],
+    8:  ['O',  1.77, 'rgb(212,000,000)', 'rgb(255,085,085)', 'black',  .00],
+    9:  ['F',  1.80, 'rgb(098,146,000)', 'rgb(143,160,085)', 'black',  .05],
+    14: ['Si', 1.85, 'rgb(215,254,220)', 'rgb(200,200,200)', 'black', -.05],
+    15: ['P',  1.95, 'rgb(010,154,000)', 'rgb(046,189,070)', 'black',  .05],
+    16: ['S',  2.15, 'rgb(238,206,000)', 'rgb(253,196,000)', 'black',  .00],
+    17: ['Cl', 2.25, 'rgb(140,205,000)', 'rgb(143,200,085)', 'black', -.10],
+    26: ['Fe', 2.25, 'rgb(140,140,140)', 'rgb(200,200,209)', 'black', -.10],
+    35: ['Br', 2.35, 'rgb(230,085,000)', 'rgb(243,160,085)', 'black', -.10],
+    53: ['I',  2.55, 'rgb(175,000,205)', 'rgb(215,000,165)', 'black',  .10],
     }
 
 partial_charge = {
@@ -37,12 +41,17 @@ bond_type = {
     'TRIPLE':3
     }
 
+bond_arrange = {
+        1: [0],
+        2: [-1, 1],
+        3: [-1.5, 0, 1.5]
+        }
+
 
 class atom:
     """ Represents an atom """
 
-    def __init__(self, id, element, radius, pos, scale_factor=1.0):
-        self.id = id
+    def __init__(self, element, radius, pos, scale_factor=1.0):
         self.element = element
         self.radius = radius
         self.pos = pos
@@ -51,22 +60,21 @@ class atom:
 
     def rotate(self, anchor, angle):
         relative_pos = self.pos - anchor
-        self.pos = np.dot(rotate_matrix(angle), relative_pos) + anchor 
+        self.pos = np.dot(rotate_matrix(angle), relative_pos) + anchor
 
-    def scale(self, N):
-        self.radius = self.radius * N
-        self.font_size = self.radius / 2.5
-        self.pos = np.dot(scale_matrix(N), self.pos)
+    def scale_pos(self, anchor, N):
+        self.new_pos = np.dot(scale_matrix(N), self.pos-anchor)
+        self.pos = self.new_pos + anchor
 
     def mirror(self, angle):
         self.pos = np.dot(mirror_matrix(angle), self.pos)
 
     def draw(self, canvas):
         constants = atom_dict[self.element]
-        [atomic_num, rad, main_color,
+        [element_symbol, rad, main_color,
          grad_color, font_color, font_xoffset] = constants[:6]
 
-        rad = self.radius * self.scale_factor / 3.0
+        rad *= self.radius * self.scale_factor / 3.0
         gradients = [
                      {
                       'grad': canvas.defs.add(canvas.linearGradient((0,0), (0,1))),
@@ -103,34 +111,117 @@ class atom:
 
         radius_vec = np.array([-self.radius/7.0, self.radius/7.0])
         label_pos = self.pos + radius_vec
-        canvas.add(canvas.text(self.element,
+        canvas.add(canvas.text(element_symbol,
                                insert=label_pos,
                                font_family = 'Arial',
                                font_size = self.font_size,
                                font_weight = 'bold',
                                fill = font_color))
 
+
+class bond:
+    def __init__(self, atom1, atom2, type, scale_factor):
+        self.atom1 = atom1
+        self.atom2 = atom2
+        self.type = type
+        self.scale_factor = scale_factor / 10.0
+
+    def draw(self, canvas):
+        dir_vector = self.atom2.pos - self.atom1.pos
+        perp_vector = perp_2d_vec(dir_vector) * self.scale_factor
+        lines = bond_arrange[self.type]
+
+        for separation_factor in lines:
+            start = self.atom1.pos + perp_vector * separation_factor
+            end   = self.atom2.pos + perp_vector * separation_factor
+            canvas.add(canvas.line(start = start,
+                                   end   = end,
+                                   stroke = 'black',
+                                   stroke_width = 5))
+
 class molecule:
     """ Represents a molecule """
 
-    def __init__(self, atoms):
+    def __init__(self,
+                 atoms=[], atom_radii=100.0,
+                 bonds=[], scale_factor=1.0):
         self.atoms = atoms
+        self.atom_radii = atom_radii
+        self.bonds = bonds
+        self.scale_factor = scale_factor
+        self.center = None
 
-    def rotate(self, anchor, angle):
+    def create_from_smiles(self, smiles_code):
+        # Creating base rdkit molecule object
+        m = Chem.MolFromSmiles(smiles_code)
+        m = AllChem.AddHs(m, False, False)
+        confID = AllChem.Compute2DCoords(m, False, True)
+        conf = m.GetConformer(confID)
+        AllChem.WedgeMolBonds(m, conf)
+        num_atoms= m.GetNumAtoms()
+
+        # Getting all atomic coordinates and transforming
+        for i in range(num_atoms):
+            element = m.GetAtomWithIdx(i).GetAtomicNum()
+            pos = np.array([m.GetConformer().GetAtomPosition(i).x,
+                            m.GetConformer().GetAtomPosition(i).y])
+            new_atom = atom(element, self.atom_radii, pos)
+            self.atoms.append(new_atom)
+        self.calculate_center()
+        self.scale(self.scale_factor)
+
+        # Getting all bonds with types
+        for mbond in m.GetBonds():
+            atom1 = self.atoms[mbond.GetBeginAtomIdx()]
+            atom2 = self.atoms[mbond.GetEndAtomIdx()]
+            type = mbond.GetBondTypeAsDouble()
+            new_bond = bond(atom1, atom2, type, self.scale_factor)
+            self.bonds.append(new_bond)
+
+    def rotate(self, angle):
         for atom in self.atoms:
-            atom.rotate(anchor, angle)
+            atom.rotate(self.center, angle)
 
     def scale(self, N):
         for atom in self.atoms:
-            atom.scale(N)
+            atom.scale_pos(self.center, N)
 
     def mirror(self, angle):
         for atom in self.atoms:
             atom.mirror(angle)
 
     def draw(self, canvas):
+        for bond in self.bonds:
+            bond.draw(canvas)
         for atom in self.atoms:
             atom.draw(canvas)
+
+    def find_minmax_points(self):
+        min_x, max_x = 1E10, -1
+        min_y, max_y = 1E10, -1
+        for i, atom in enumerate(self.atoms):
+            if atom.pos[0] < min_x:
+                min_x = atom.pos[0]
+            if atom.pos[0] > max_x:
+                max_x = atom.pos[0]
+            if atom.pos[1] < min_y:
+                min_y = atom.pos[1]
+            if atom.pos[1] > max_y:
+                max_y = atom.pos[1]
+        return [min_x, max_x, min_y, max_y]
+
+    def calculate_center(self):
+        minmax = self.find_minmax_points()
+        self.c = np.array([[minmax[1]-minmax[0]],
+                           [minmax[3]-minmax[2]]])
+        self.center = self.c.transpose().flatten()
+
+    def set_new_center(self, new_center):
+        trans = new_center - self.center
+        for atom in self.atoms:
+            atom.pos = atom.pos + trans
+        self.calculate_center()
+
 
 def create_canvas(filename, size, debug=False):
     """ Returns an svg canvas object """
--- moldraw.py
+++ moldraw.py
@@ -14,13 +14,9 @@ import svgwrite
 import numpy as np
 
 if __name__ == '__main__':
-    print('hi')
-    canvas = create_canvas('test', (600, 600))
-    H1 = atom(0, 'H', 150.0, np.array([100, 500]))
-    H2 = atom(1, 'H', 150.0, np.array([450, 350]))
-    O =  atom(2, 'O', 150.0, np.array([150, 50]))
-    N =  atom(3, 'N', 150.0, np.array([250, 400]))
-    mol = molecule([H1, H2, O, N])
-    mol.rotate(np.array([300, 300]), np.pi)
+    mol = molecule(scale_factor=105.0)
+    mol.create_from_smiles('')
+    mol.rotate(np.pi/2)
+    canvas = create_canvas('test', size=[800,800])
     mol.draw(canvas)
     canvas.save()
